227ebb02f73fa95a1709ed244188d48707c76b5f
ename12 <2725402440@qq.com>
2020-08-25 10:28:27 +0800

    解决冲突

--- system/src/components/user/usList.vue
+++ system/src/components/user/usList.vue
@@ -1,44 +1,19 @@
 <template>
   <div>
-    <el-table
-      ref="multipleTable"
-      :data="tableData"
-      tooltip-effect="dark"
-      style="width: 100%"
-      @selection-change="handleSelectionChange"
-    >
-      <el-table-column type="selection" width="55"></el-table-column>
-      <el-table-column label="序号" width="120" prop="id"></el-table-column>
-      <el-table-column prop="username" label="用户名" width="120"></el-table-column>
-      <el-table-column prop="age" label="年龄" show-overflow-tooltip></el-table-column>
-    </el-table>
   </div>
 </template>
 
 <script>
-import test from "@/api/test";
 
 export default {
   data() {
     return {
-      tableData: [],
+      
     };
   },
   components: {},
 
   methods: {
-    async git() {
-      try {
-        let { data } = await test.getList();
-        this.tableData = data.data;
-        console.log("请求成功", data.data);
-      } catch (error) {
-        console.log("错误为", error);
-      }
-    },
-  },
-  created() {
-    this.git();
   },
 };
 </script>
