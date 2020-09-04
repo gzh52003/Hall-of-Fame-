8b299562e65e9490b3c74f9ea8d1fb530204cecf
1939804418 <1939804418@qq.com>
2020-09-04 20:40:07 +0800

    提交

--- system-mysql/api/signin.js
+++ system-mysql/api/signin.js
@@ -6,6 +6,35 @@ const router = Router();  //启动路由
 const request = require('../mysql');  //引入数据库方法
 
 //查询用户名是否存在
+// router.get('/:id',(req,res)=>{
+//     res.send('这是请求的数据');
+// })
+// //添加用户 用body
+// router.post('/',(req,res)=>{  //req：接受前端数据
+//   let {name} = req.body;  //1、接收前端body数据
+//   res.send(name);
+// })
+// //删除用户  用/:id，动态路由
+// router.delete('/:id',(req,res)=>{  //req：接受前端数据
+//  const p = req.params;    //2、接受前端动态路由
+//  res.send({
+//    code:200,
+//    text:'请求成功',
+//    p
+//  })
+// })
+
+// //修改数据
+// router.put('/',(req,res)=>{  //req：接受前端数据
+//   const p = req.query;    //接受路由后边?a=12&b=d
+//  res.send({
+//   code:200,
+//   text:'请求成功',
+//   p
+//  });
+// })
+
+
 router.get('/:username', async (req, res) => {
   let info;
   const { username } = req.params;
@@ -33,7 +62,7 @@ router.get('/:username', async (req, res) => {
       meg: '查询失效'
     }
   }
-  res.send(info);
+ res.send(info);
 })
 
 module.exports = router;
